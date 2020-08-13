8bb44bb6ee4022d57c665ec5fd0e2febf1b6a126
KonstantinMalkov <mkv.kasatin@gmail.com>
2020-08-13 12:46:57 +0500

    Initial commit

---
+++ src/main/java/ru/geekbrains/sample/controllers/BookController.java
@@ -0,0 +1,12 @@
+package ru.geekbrains.sample.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class BookController {
+
+
+
+}
