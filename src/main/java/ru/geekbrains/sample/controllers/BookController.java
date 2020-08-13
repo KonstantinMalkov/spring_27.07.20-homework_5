388c571f4c58358cf67b402f6f5fc18fdb5544cb
KonstantinMalkov <mkv.kasatin@gmail.com>
2020-08-13 13:26:47 +0500

    практическое задание к уроку 5
    практическое задание к уроку 5

--- src/main/java/ru/geekbrains/sample/controllers/BookController.java
+++ src/main/java/ru/geekbrains/sample/controllers/BookController.java
@@ -2,11 +2,22 @@ package ru.geekbrains.sample.controllers;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import ru.geekbrains.sample.services.BookService;
+
+import java.util.UUID;
 
 @Controller
 @RequiredArgsConstructor
 public class BookController {
 
+    public final BookService bookService;
 
-
+    @GetMapping("/books/{id}")
+    public String getBookPage(@PathVariable String id, Model model) {
+        model.addAttribute("book", bookService.getOne(UUID.fromString(id)));
+        return "book";
+    }
 }
--- src/main/java/ru/geekbrains/sample/services/BookService.java
+++ src/main/java/ru/geekbrains/sample/services/BookService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import ru.geekbrains.sample.persistence.entities.Book;
 import ru.geekbrains.sample.persistence.repositories.BookRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -22,6 +23,18 @@ public class BookService {
 
     public Book getOne(UUID id) {
         return bookRepository.findById(id).orElse(null);
+    }
+
+    public List<Book> getCountBooks(int firstNumber, int count) {
+        List<Book> listFromBase = bookRepository.findAll();
+        List<Book> returnList = new ArrayList<>();
+
+        for (int i = firstNumber; i <= count; i++) {
+            if(i <= listFromBase.size()){
+                returnList.add(listFromBase.get(i));
+            }
+        }
+        return returnList;
     }
 
 }
